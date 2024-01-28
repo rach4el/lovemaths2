ed69ed195cfb3de17b265d41efc58b87af53e443
rachael baikey <rachaelacb@gmail.com>
2024-01-28 14:09:28 +0000

    scipt1

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+
+
+
+
+
