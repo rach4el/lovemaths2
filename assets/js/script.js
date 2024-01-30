abebe7fa5a3047190abc976117fc2aad6f8612bb
rachael baikey <rachaelacb@gmail.com>
2024-01-30 16:38:45 +0000

    idk

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,21 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
+  
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
+
+
+
 
 }
 
