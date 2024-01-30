4a471064dc4263b7d9809f47b40994a6692d073c
rachael baikey <rachaelacb@gmail.com>
2024-01-30 19:52:29 +0000

    res

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,6 +145,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 function displayDivisionQuestion(operand1, operand2) {
 
+    let result = operand2 * result
+    
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
