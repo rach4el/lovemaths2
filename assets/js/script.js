8f2ded97e36d0d6e8cfaa5fc4666a35f122fc563
rachael baikey <rachaelacb@gmail.com>
2024-02-02 17:16:53 +0000

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,11 +46,7 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division" ) {
         displayDivisionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
-
+    } 
 }
 
 /**
@@ -143,15 +139,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 
 }
-function displayDivisionQuestion(operand1, operand2) {
-    
-    num1 = num1 * num2;
+ function displayDivisionQuestion() {
+    let num2 = Math.floor(Math.random() * 24) + 2; 
+    let quotient = Math.floor(Math.random() * 25) + 1; 
+    let num1 = num2 * quotient;
 
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "/";
-
-}
-
-
-
+    document.getElementById("operand1").textContent = num1;
+    document.getElementById("operand2").textContent = num2;
+    document.getElementById("operator").textContent = "/";
+ }
